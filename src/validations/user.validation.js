6376a9267634b35983811a40fd1b94ae8a2782fb
bankonly <for21bn01@gmail.com>
2020-12-29 11:16:00 +0700

    s

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -7,9 +7,9 @@ const { _, RemoveSpace } = require("../utils/common-func");
 const UserValidator = {
   register: Catcher(async (req, res, next) => {
     const schema = Joi.object({
-      email: Joi.string().required().email(),
-      username: Joi.string().required().min(5),
-      password: Joi.string().required().min(5),
+      email: Joi.string().required().email().trim(),
+      username: Joi.string().required().min(5).trim(),
+      password: Joi.string().required().min(5).trim(),
       confirm_password: Joi.any().valid(Joi.ref("password")).required(),
     });
     await schema.validateAsync(req.body);
@@ -25,13 +25,13 @@ const UserValidator = {
   login: Catcher(async (req, res, next) => {
     const body = req.body;
     const schema = Joi.object({
-      username: Joi.string().required().min(5),
-      password: Joi.string().required().min(5),
+      username: Joi.string().required().min(5).trim(),
+      password: Joi.string().required().min(5).trim(),
     });
 
-    req.body.username = _.removeSpace(req.body.username);
-
     await schema.validateAsync(body);
+
+    req.body.username = _.removeSpace(req.body.username);
     const found_user = await Mongo.find(UserModel, { error_code: 400, throw_error: true, many: false, condition: { username: body.username } });
     const verfify_password = await _.bcryptFn.verifyPassword(body.password, found_user.password);
     if (!verfify_password) throw new Error(`400::invalid password`);
